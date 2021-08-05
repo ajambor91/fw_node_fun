1301895cfd60dd355ec4296db243661aaf7e7138
Adam Jambor <adam91mko@gmail.com>
2021-08-05 16:57:10 +0200

    fixes

--- src/core/decorator/routing/routing.ts
+++ src/core/decorator/routing/routing.ts
@@ -6,7 +6,6 @@ export const Controller = (prefix: string = ''): ClassDecorator => {
        const args = Reflect.getMetadata('design:paramtypes', target) || [];
        // const resArgs = args.map((param: any) => {})
         Reflect.defineMetadata('prefix', prefix, target);
-        // Since routes are set by our methods this should almost never be true (except the controller has no methods)
         if (! Reflect.hasMetadata('routes', target)) {
             Reflect.defineMetadata('routes', [], target);
         }
@@ -15,15 +14,11 @@ export const Controller = (prefix: string = ''): ClassDecorator => {
 
 
 export const Get = (path: string): MethodDecorator => {
-    // `target` equals our class, `propertyKey` equals our decorated method name
     return (target, propertyKey: string): void => {
-        // In case this is the first route to be registered the `routes` metadata is likely to be undefined at this point.
-        // To prevent any further validation simply set it to an empty array here.
+
         if (! Reflect.hasMetadata('routes', target.constructor)) {
             Reflect.defineMetadata('routes', [], target.constructor);
         }
-
-        // Get the routes stored so far, extend it by the new route and re-set the metadata.
         const routes = Reflect.getMetadata('routes', target.constructor) as Array<IMethods>;
 
         routes.push({
@@ -36,9 +31,9 @@ export const Get = (path: string): MethodDecorator => {
 };
 
 
-export const Routing = function () {
-    return true;
-}
+// export const Routing = function () {
+//     return true;
+// }
 export const get = (val: any) => (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => {
 
 };
